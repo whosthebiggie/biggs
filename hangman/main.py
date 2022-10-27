a6a62bf1b9b9814cab84b4e7888aceafbabaed28
Ethan Maldonado <ethaninrgv@gmail.com>
2022-10-27 01:14:20 -0500

    Update Hangman project with random answers

--- hangman/main.py
+++ hangman/main.py
@@ -1,7 +1,19 @@
-import re
+import re 
+import random
 
-# Get the answer.
-answer = "What's Up, Doc?"
+# Get the answer. 
+pool_file = open ("hangman-sample-answer-pool.txt") 
+ 
+pool_answers = []  
+ 
+pool_answer_line = pool_file.readline() 
+ 
+while pool_answer_line: 
+    pool_answers.append(pool_answer_line) 
+     
+    pool_answer_line = pool_file.readline() 
+
+answer = random.choice(pool_answers)
 
 answer = answer.upper()
 
